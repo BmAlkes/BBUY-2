f8709a90a07641df48f66ce76c6fb2f256673e49
bmalkes <bmalkes@gmail.com>
2023-03-07 16:07:18 +0200

    feat:add reset Page

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./auth.module.scss";
+import resetImg from "../../assets/forgot.png";
+import { Link } from "react-router-dom";
+import { BsGoogle } from "react-icons/bs";
 
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={resetImg} alt="login" />
+      </div>
+      <div className={`${styles.form} --card`}>
+        <h2>Reset Password</h2>
+        <form>
+          <input type="text" placeholder="Email" required />
+          <button className="--btn --btn-primary --btn-block">
+            Reset password
+          </button>
+          <div className={styles.links}>
+            <Link to="/login">-Login</Link>
+            <Link to="/register">-Register</Link>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Reset;
