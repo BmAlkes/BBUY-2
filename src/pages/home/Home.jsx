3c8f6161c36bd40b5ccb6bb8faf7900504b01209
bmalkes <bmalkes@gmail.com>
2023-03-12 16:22:42 +0200

    feat:add scroll smooth

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Products from "../../components/products/Products";
 import Slider from "../../components/slider/Slider";
 
 const Home = () => {
+  const url = window.location.href;
+  const scrollSection = () => {
+    if (url.includes("#products")) {
+      window.scrollTo({
+        top: 1200,
+        behavior: "smooth",
+      });
+      return;
+    }
+  };
+
+  useEffect(() => {
+    scrollSection();
+  }, []);
   return (
     <div>
       <Slider />
