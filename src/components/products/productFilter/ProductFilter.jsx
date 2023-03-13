45d496d8e81d91733e8c65d211870a0037a318ab
bmalkes <bmalkes@gmail.com>
2023-03-13 14:49:28 +0200

    feat:brand filter

--- src/components/products/productFilter/ProductFilter.jsx
+++ src/components/products/productFilter/ProductFilter.jsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+} from "../../../store/slice/filterSlice";
 import styles from "./productFilter.module.scss";
 
 const ProductFilter = () => {
+  const [category, setCategories] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const products = useSelector((state) => state.product.products);
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+  const allBrands = [
+    "All",
+    ...new Set(products.map((product) => product.brand)),
+  ];
+
+  const filteredProducts = (cat) => {
+    setCategories(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
+  useEffect(() => {
+    dispatch(FILTER_BY_BRAND({ products, brand }));
+  }, [products, brand]);
+
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        <button>All</button>
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`${category}` === cat ? `${styles.active}` : null}
+              onClick={() => filteredProducts(cat)}
+            >
+              &#8250;{cat}
+            </button>
+          );
+        })}
       </div>
       <h4>Brand</h4>
       <div className={styles.brand}>
-        <select name="brand">
-          <option value="All">All</option>
+        <select value={brand} onChange={(e) => setBrand(e.target.value)}>
+          {allBrands.map((brand, index) => {
+            return (
+              <option key={index} value={brand}>
+                {brand}
+              </option>
+            );
+          })}
         </select>
         <h4>Price</h4>
         <p>1500</p>
