7b52215d57ed9dffa163b0d3d5669f7937e9fe47
bmalkes <bmalkes@gmail.com>
2024-01-26 17:59:23 +0200

    fix:troubleshoot

--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -1,31 +1,31 @@
 export const sliderData = [
   {
     image:
-      "https://res.cloudinary.com/binvent/image/upload/v1678285968/BInvent%20App/bg-4_xliv1v.jpg",
+      "https://res.cloudinary.com/landingpage2/image/upload/v1678285968/BInvent%20App/bg-4_xliv1v.jpg",
     heading: "Shoes",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
     image:
-      "https://res.cloudinary.com/binvent/image/upload/v1678285968/BInvent%20App/bg-1_m08cle.jpg",
+      "https://res.cloudinary.com/landingpage2/image/upload/v1678285968/BInvent%20App/bg-1_m08cle.jpg",
     heading: "Women Fashion",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
     image:
-      "https://res.cloudinary.com/binvent/image/upload/v1678285962/BInvent%20App/bg-2_i3f1ag.jpg",
+      "https://res.cloudinary.com/landingpage2/image/upload/v1678285962/BInvent%20App/bg-2_i3f1ag.jpg",
     heading: "Men Fashion",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
     image:
-      "https://res.cloudinary.com/binvent/image/upload/v1678285962/BInvent%20App/bg-3_sgnw6b.jpg",
+      "https://res.cloudinary.com/landingpage2/image/upload/v1678285962/BInvent%20App/bg-3_sgnw6b.jpg",
     heading: "Awesome Gadgets",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
     image:
-      "https://res.cloudinary.com/binvent/image/upload/v1678285962/BInvent%20App/xbox_wdow6s.webp",
+      "https://res.cloudinary.com/landingpage2/image/upload/v1678285962/BInvent%20App/xbox_wdow6s.webp",
     heading: "Best Games",
     desc: "Up to 30% off on all onsale proucts.",
   },
